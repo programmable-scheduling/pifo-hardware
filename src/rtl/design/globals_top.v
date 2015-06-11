851e4908e73d8fb8dc0597a9e7637ffe957cda14
Suvinay Subramanian <suvinay@csail.mit.edu>
2015-06-11 03:13:54 -0400

    [design] Add pifo core logic, and pifo top level module.

--- src/rtl/design/globals_top.v
+++ src/rtl/design/globals_top.v
@@ -7,8 +7,12 @@ timeprecision 1ps;
 
 package PifoPkg;
 
-localparam      MAX_PACKET_PRIORITY     =   (256);  // Packet priorities should be in the range [0, MAX_PACKET_PRIORITY) (integers)
-localparam      PACKET_POINTER_WIDTH    =   (8);
+localparam  MAX_PACKET_PRIORITY     =   (256);  // Packet priorities should be in the range [0, MAX_PACKET_PRIORITY) (integers)
+localparam  PRIORITY_WIDTH          =   $clog2(MAX_PACKET_PRIORITY);  
+localparam  PACKET_POINTER_WIDTH    =   (8);
+localparam  NUM_ELEMENTS            =   (16);
+
+typedef logic [PACKET_POINTER_WIDTH-1:0]    PacketPointer;
 
 endpackage
 
--- src/rtl/design/pifo_base.v
+++ src/rtl/design/pifo_base.v
@@ -35,6 +35,7 @@ parameter   DATA_WIDTH      = (8);
 // Constants
 //------------------------------------------------------------------------------
 localparam  PRIO_WIDTH      = $clog2(MAX_PRIORITY);
+localparam  IDX_WIDTH       = $clog2(NUM_ELEMENTS);
 
 
 //------------------------------------------------------------------------------
@@ -75,6 +76,7 @@ input  logic                        i__clear_all;
 //------------------------------------------------------------------------------
 logic                               w__push;
 logic                               w__pop;
+logic           [IDX_WIDTH-1:0]     w__enq_idx; 
 
 //------------------------------------------------------------------------------
 // States and next signals
@@ -83,6 +85,8 @@ logic                               w__empty__next;
 logic                               r__empty__pff;
 logic                               w__full__next;
 logic                               r__full__pff;
+logic           [IDX_WIDTH-1:0]     r__pifo_count__pff;
+logic           [IDX_WIDTH-1:0]     w__pifo_count__next; 
 
 PifoEntry                           r__buffer__pff          [NUM_ELEMENTS-1:0];
 
@@ -92,14 +96,8 @@ PifoEntry                           r__buffer__pff          [NUM_ELEMENTS-1:0];
 assign o__data_in_ready         = ~r__full__pff & (~reset);         // When reset, ready should be low
 assign o__data_in_ready__next   = ~w__full__next;
 assign o__data_out_valid        = ~r__empty__pff;                   // When reset, this _will_ be low
-assign o__data_out              = r__buffer_pff[0].data;
-assign o__data_out_priority     = r__buffer_pff[0].prio;
-
-
-//------------------------------------------------------------------------------
-// Submodules
-//------------------------------------------------------------------------------
-
+assign o__data_out              = r__buffer__pff[0].data;
+assign o__data_out_priority     = r__buffer__pff[0].prio;
 
 //------------------------------------------------------------------------------
 // Internal push and pop signals
@@ -114,8 +112,34 @@ begin
 end
 
 //------------------------------------------------------------------------------
-// Internal full and empty states
+// Pifo element count 
 //------------------------------------------------------------------------------
+always_comb
+begin
+    w__pifo_count__next = r__pifo_count__pff;
+    if (w__push)
+    	w__pifo_count__next = r__pifo_count__pff + 1'b1;
+    else if (w__pop)
+    	w__pifo_count__next = r__pifo_count__pff - 1'b1;
+end
+
+always_ff @(posedge clk)
+begin
+    if ((reset == 1'b1) || (i__clear_all == 1'b1))
+    	r__pifo_count__pff <= '0;
+    else
+        r__pifo_count__pff <= w__pifo_count__next;
+end
+
+//------------------------------------------------------------------------------
+// Internal full and empty states, signals
+//------------------------------------------------------------------------------
+always_comb
+begin
+    w__empty__next  = (w__pifo_count__next == '0);
+    w__full__next   = (w__pifo_count__next == NUM_ELEMENTS);
+end
+
 always_ff @ (posedge clk)
 begin
     if(reset == 1'b1)
@@ -129,5 +153,48 @@ begin
         r__full__pff    <= w__full__next;
     end
 end
+
+//------------------------------------------------------------------------------
+// Pifo next state : Core PIFO logic
+//------------------------------------------------------------------------------
+// Enqueue index computation
+integer idx;
+always_comb
+begin
+    w__enq_idx = '0;
+    for(idx = 0; idx < NUM_ELEMENTS; idx = idx + 1)
+    	if ((idx < r__pifo_count__pff) && (i__data_in_priority > r__buffer__pff[idx].prio))
+    		w__enq_idx = idx;
+end
+
+genvar pifo_idx;
+generate for(pifo_idx = 0; pifo_idx < NUM_ELEMENTS; pifo_idx = pifo_idx + 1) 
+begin: gen_pifo_next_state
+    PifoEntry   r__buffer__next;
+    always_comb
+    begin
+        r__buffer__next = r__buffer__pff[pifo_idx];
+        if (!w__push && w__pop && (pifo_idx != NUM_ELEMENTS-1))
+        begin 
+            r__buffer__next = r__buffer__pff[pifo_idx+1];
+        end
+        else if (w__push && !w__pop) 
+        begin
+            if (pifo_idx > w__enq_idx)
+            	r__buffer__next = r__buffer__pff[pifo_idx - 1];
+            else if (pifo_idx == w__enq_idx)
+            	r__buffer__next = r__buffer__pff[pifo_idx - 1];
+        end
+        // [ssub] For now, either 1 push or 1 pop (or neither) can
+        // happen in any particular cycle. If this assumption is violated
+        // bad things will happen.
+    end
+
+    always_ff @(posedge clk)
+    begin
+        r__buffer__pff[pifo_idx] <= r__buffer__next;
+    end
+end
+endgenerate
 
 endmodule
